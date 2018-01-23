56f3c547f438a7c87fb5c3fa427b9b86875d879e
Raghu Betina <raghu@firstdraft.com>
2018-01-23 20:29:26 +0000

    Generate listings resource with starter:resource

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,83 @@
+class ListingsController < ApplicationController
+  def index
+    @listings = Listing.all
+
+    render("listings/index.html.erb")
+  end
+
+  def show
+    @listing = Listing.find(params[:id])
+
+    render("listings/show.html.erb")
+  end
+
+  def new
+    @listing = Listing.new
+
+    render("listings/new.html.erb")
+  end
+
+  def create
+    @listing = Listing.new
+
+    @listing.address = params[:address]
+    @listing.lat = params[:lat]
+    @listing.lng = params[:lng]
+
+    save_status = @listing.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/listings/new", "/create_listing"
+        redirect_to("/listings")
+      else
+        redirect_back(:fallback_location => "/", :notice => "Listing created successfully.")
+      end
+    else
+      render("listings/new.html.erb")
+    end
+  end
+
+  def edit
+    @listing = Listing.find(params[:id])
+
+    render("listings/edit.html.erb")
+  end
+
+  def update
+    @listing = Listing.find(params[:id])
+
+    @listing.address = params[:address]
+    @listing.lat = params[:lat]
+    @listing.lng = params[:lng]
+
+    save_status = @listing.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/listings/#{@listing.id}/edit", "/update_listing"
+        redirect_to("/listings/#{@listing.id}", :notice => "Listing updated successfully.")
+      else
+        redirect_back(:fallback_location => "/", :notice => "Listing updated successfully.")
+      end
+    else
+      render("listings/edit.html.erb")
+    end
+  end
+
+  def destroy
+    @listing = Listing.find(params[:id])
+
+    @listing.destroy
+
+    if URI(request.referer).path == "/listings/#{@listing.id}"
+      redirect_to("/", :notice => "Listing deleted.")
+    else
+      redirect_back(:fallback_location => "/", :notice => "Listing deleted.")
+    end
+  end
+end
